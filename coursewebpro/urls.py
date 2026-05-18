b2c367bfc5c86391c0e7440ab5c5c3dbdd7d78c7
Vineethkumarreddy2486 <vineethkumarreddy2486@gmail.com>
2026-05-18 16:32:47 +0530

    debug changed

--- coursewebpro/urls.py
+++ coursewebpro/urls.py
@@ -35,5 +35,6 @@ urlpatterns = [
     path('delcart/<int:id>',Cart_delete,name='delcart'),
     path('search/',search_bar,name='search')
 ]
-urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 # urlpatterns+=static(settings.STATIC_URL,document_root=settings.STATIC_DIR)   not needed because we are using whitenoise to serve static files in production.
