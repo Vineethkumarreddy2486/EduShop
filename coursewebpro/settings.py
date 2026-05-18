c20133a7243da752edc953c068011e15da76eec5
Vineethkumarreddy2486 <vineethkumarreddy2486@gmail.com>
2026-05-18 15:59:26 +0530

    debug changes

--- coursewebpro/settings.py
+++ coursewebpro/settings.py
@@ -24,9 +24,9 @@ STATIC_DIR=os.path.join(BASE_DIR,'static')
 SECRET_KEY = 'django-insecure-97##xc!1yn%_y6$=ybz%e_qum+)q*61pxl+04wmy&@b@z#hbyt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.onrender.com']
 
 
 # Application definition
